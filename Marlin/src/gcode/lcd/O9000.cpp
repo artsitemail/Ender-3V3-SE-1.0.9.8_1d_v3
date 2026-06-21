d058a0c2722a5610debc755e7d83a39a2ed1a7b1
Artsite <artsitemail@gmail.com>
2026-06-21 18:06:12 +0100

    Initial commit

---
+++ Marlin/src/gcode/lcd/O9000.cpp
@@ -0,0 +1,10 @@
+#include "../../inc/MarlinConfig.h"
+#include "../gcode.h"
+#include "../parser.h"
+#include "../../lcd/dwin/e3v2/dwin.h"
+#include "../../lcd/marlinui.h"
+
+void GcodeSuite::O9000() {
+  // Função silenciada: Não faz nada, apenas previne erros de compilação
+  return;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#Advanced Memory Usage is available via "PlatformIO Home > Project Inspect"
+#RAM:   [=====     ]  49.6% (used 32536 bytes from 65536 bytes)
+#Flash: [=====     ]  48.4% (used 253764 bytes from 524288 bytes)
